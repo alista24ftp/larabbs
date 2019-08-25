cba1acf590aeff0110365024fbe2435318b1fc64
John <johnlittlefish@hotmail.com>
2019-08-25 02:01:02 +0000

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
 
   <!-- Styles -->
   <link href="{{mix('css/app.css')}}" rel="stylesheet">
+
+  @yield('styles')
+
 </head>
 <body>
   <div id="app" class="{{route_class()}}-page">
@@ -28,5 +31,7 @@
 
   <!-- Scripts -->
   <script src="{{mix('js/app.js')}}"></script>
+
+  @yield('scripts')
 </body>
 </html>
